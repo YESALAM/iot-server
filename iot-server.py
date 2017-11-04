5c1eca54b2cf47a70996683524c979df0e631942
yesalam <itisalam@gmail.com>
2017-11-04 19:49:37 +0530

    selecting database before creating table

--- iot-server.py
+++ iot-server.py
@@ -18,6 +18,8 @@ cur = db.cursor()
 def createTable():
     cur.execute("""CREATE DATABASE IF NOT EXISTS  iot""")
     db.commit()
+    cur.execute("""USE iot""")
+    db.commit()
     sql_create  = "CREATE TABLE IF NOT EXISTS iotdata (id int(11) NOT NULL AUTO_INCREMENT, dates varchar(10), times varchar(10), uuid varchar(10),name varchar(25),vrn varchar(10),purpose varchar(50),nop varchar(10),access varchar(10),active boolean,PRIMARY KEY (id));"
     cur.execute(sql_create)
     db.commit()
