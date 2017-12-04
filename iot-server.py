a0e17b3c2affeee4e9e0d38053d29759886ee552
yesalam <itisalam@gmail.com>
2017-12-04 22:50:05 +0530

    uuid can be of 12 digit

--- iot-server.py
+++ iot-server.py
@@ -20,7 +20,7 @@ def createTable():
     db.commit()
     cur.execute("""USE iot""")
     db.commit()
-    sql_create  = "CREATE TABLE IF NOT EXISTS iotdata (id int(11) NOT NULL AUTO_INCREMENT, dates varchar(10), times varchar(10), uuid varchar(10),name varchar(25),vrn varchar(10),purpose varchar(50),nop varchar(10),access varchar(10),active boolean,PRIMARY KEY (id));"
+    sql_create  = "CREATE TABLE IF NOT EXISTS iotdata (id int(11) NOT NULL AUTO_INCREMENT, dates varchar(10), times varchar(10), uuid varchar(12),name varchar(25),vrn varchar(10),purpose varchar(50),nop varchar(10),access varchar(10),active boolean,PRIMARY KEY (id));"
     cur.execute(sql_create)
     db.commit()
 
